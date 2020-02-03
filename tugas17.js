21c97476b9a3548c9296c3101884f2e09ce61f57
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:23:38 +0700

    Tugas 17 JavaScript
    Pengumpulan tugas 17 JavaScript Niomic

---
+++ tugas17.js
@@ -0,0 +1,12 @@
+var bag1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+console.log(bag1.toLowerCase());
+console.log(bag1.toUpperCase());
+var bag2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(bag2.toLowerCase());
+console.log(bag2.toUpperCase());
+
+console.log();
+
+var bag3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+console.log(bag3.charCodeAt(0),"-",bag3.charCodeAt(1),"-",bag3.charCodeAt(2),"-",bag3.charCodeAt(3),"-",bag3.charCodeAt(4),"-",bag3.charCodeAt(5));
